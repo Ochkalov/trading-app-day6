12c4dca27b46c2eb16c34e1905db463653a93089
oyushche <uhmateush@gmail.com>
2017-11-03 10:48:58 -0400

    day6-v16 trader-details component tests

---
+++ src/app/testing/router-stubs.spec.ts
@@ -0,0 +1,31 @@
+
+
+
+import {Injectable} from "@angular/core";
+import {Subject} from "rxjs/Subject";
+import {convertToParamMap, Params} from "@angular/router";
+
+@Injectable()
+export class ActivatedRouteStub
+{
+  private subject = new Subject();
+  paramMap = this.subject.asObservable();
+
+  private _testParamMap: Params;
+
+  get testParamMap()
+  {
+    return this._testParamMap;
+  }
+
+  set testParamMap(params: Params)
+  {
+    this._testParamMap = params;
+    this.subject.next(convertToParamMap(this._testParamMap));
+  }
+
+  get snapshot()
+  {
+    return { paramMap: this.testParamMap };
+  }
+}
--- src/app/trader-details/trader-details.component.spec.ts
+++ src/app/trader-details/trader-details.component.spec.ts
@@ -1,25 +1,92 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TraderDetailsComponent } from './trader-details.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ReactiveFormsModule} from "@angular/forms";
+import {StocksComponent} from "./stocks/stocks.component";
+import {TradersService} from "../traders/traders.service";
+import {MarketServiceImpl} from "../market/market.service";
+import {ActivatedRoute} from "@angular/router";
+import {MarketServiceSpy} from "../testing/market.service.spy.spec";
+import {Location, LocationStrategy} from "@angular/common";
+import {LocationStrategySpy} from "../testing/location.strategy.spy.spec";
+import {ActivatedRouteStub} from "../testing/router-stubs.spec";
+import {MatAutocompleteModule, MatInputModule, MatOptionModule} from "@angular/material";
+import {Stock} from "../domain/Stock";
+import {DebugElement} from "@angular/core";
 
 describe('TraderDetailsComponent', () => {
+
+  let expectedTraderName: string = 'Oleg';
+  let activatedRoute: ActivatedRouteStub = new ActivatedRouteStub();
+
   let component: TraderDetailsComponent;
   let fixture: ComponentFixture<TraderDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(async(() =>
+  {
     TestBed.configureTestingModule({
-      declarations: [ TraderDetailsComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatAutocompleteModule,
+        MatOptionModule,
+        ReactiveFormsModule
+      ],
+      declarations: [TraderDetailsComponent, StocksComponent],
+      providers: [
+        TradersService,
+        {provide: MarketServiceImpl, useClass: MarketServiceSpy},
+        {provide: ActivatedRoute, useValue: activatedRoute},
+        Location,
+        {provide: LocationStrategy, useClass: LocationStrategySpy},
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TraderDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    activatedRoute.testParamMap = {name: expectedTraderName};
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('trader should be set', function (done)
+  {
+    setTimeout(() =>
+    {
+      expect(component.trader.getName()).toEqual(expectedTraderName);
+      done();
+
+    }, 100)
+
+  }, 200);
+
+  it('stock should be added to portfolio', function (done)
+  {
+    let expectedStockSymbol = 'XXX';
+
+    setTimeout(() =>
+    {
+      component.selectedStock = new Stock(expectedStockSymbol, '', new MarketServiceSpy());
+      component.countInput.setValue(10);
+
+      let buyStockBtn: DebugElement = fixture.debugElement.query((de: DebugElement) => de.attributes['id'] === 'buyStockBtn');
+      buyStockBtn.nativeElement.click();
+
+      let actualTrade = component.trader.getPortfolio().find(trade => expectedStockSymbol === trade.getStock().getSymbol());
+
+      expect(actualTrade.getStock().getSymbol()).toEqual(expectedStockSymbol);
+
+      done();
+
+    }, 100)
+
+  }, 200);
 });
